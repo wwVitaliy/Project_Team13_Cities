dc28431f9da40da550afb5edccb9c000fa35afd1
olexandr2009 <hristevich.ua@gmail.com>
2023-08-30 14:32:53 +0300

    task3 done added comments

---
+++ src/main/java/GUI/GUI.java
@@ -0,0 +1,16 @@
+package GUI;
+
+import GUI.windows.StartWindow;
+
+//GUI це клас який просто має створювати об'єкти класів з пакету window
+public class GUI {
+    public static void startWindow(){
+        new StartWindow();
+    }
+    public static void gameWindow(){
+
+    }
+    public static void scoreWindow(){
+
+    }
+}
