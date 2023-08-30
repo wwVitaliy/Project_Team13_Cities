53e051e09da7b5f536a91534e9adaf1bddb97731
olexandr2009 <hristevich.ua@gmail.com>
2023-08-30 21:32:28 +0300

    GameWindow done added comments,  added city Image to windows titles

--- src/main/java/GUI/GUI.java
+++ src/main/java/GUI/GUI.java
@@ -1,14 +1,15 @@
 package GUI;
 
+import GUI.windows.GameWindow;
 import GUI.windows.StartWindow;
 
-//GUI це клас який просто має створювати об'єкти класів з пакету window
+//GUI це клас який просто має створювати об'єкти класів з пакету windows
 public class GUI {
     public static void startWindow(){
         new StartWindow();
     }
     public static void gameWindow(){
-
+        new GameWindow();
     }
     public static void scoreWindow(){
 
